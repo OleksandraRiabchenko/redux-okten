5b2efc86137acb9af7bf85519cebb661b1aaf69f
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-07-07 01:39:26 +0300

    lesson1 part1 action'INC'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-export default function App() {
-  return (
-    <div >
+import {useSelector, useDispatch} from 'react-redux';
 
-    </div>
-  );
+const SomeNestedChildComponent = () => {
+    // const counter = useSelector((state) => state.counterValue); або 2й варіант одразу деструктуризувати об'єкт
+    const counter = useSelector(({counterValue}) => counterValue);
+    //console.log(counter);
+
+    return (
+        <div>
+            SOMETHING
+            <h1> {counter}</h1>
+
+        </div>
+    )
+}
+
+const SomeChildComponent = () => {
+    return (<SomeNestedChildComponent/>)
+}
+
+export default function App() {
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <button onClick={() => {
+                dispatch({type: 'INC'})
+            }
+            }>inc
+            </button>
+            <SomeChildComponent/>
+        </div>
+    );
 }
 
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {
+    counterValue: 0
+}
+const counterReduser = (state = initialState, action) => {
+    //console.log({state, action})
+    switch (action.type) {
+        case 'INC': {
+            return {...state, counterValue: state.counterValue + 1}
+        }
+        case 'DEC': {
+            return {...state, counterValue: state.counterValue - 1}
+        }
+        case 'RESET': {
+            return {...state, counterValue: 0}
+        }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(counterReduser);
+
+// console.log(store);
+// console.log('not from subscribe', store.getState())
+// store.subscribe(() => {
+//     console.log('from subscribe', store.getState())
+// })
+// store.dispatch({
+//     type:'INC'
+// })
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
