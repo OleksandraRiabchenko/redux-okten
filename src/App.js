9a54ad9adb5abcc7819f8c25a7bc8f7d2c33c3cd
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-07-08 15:15:08 +0300

    Practice1_users/posts/comments_with_redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-export default function App() {
-  return (
-    <div >
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Users from './users/Users';
+import Posts from './posts/Posts';
+import Comments from './comments/Comments';
 
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Link to={'/users'}>users page</Link><br/>
+                <Link to={'/posts'}>posts page</Link><br/>
+                <Link to={'/comments'}>comments page</Link>
+
+                <Switch>
+                    <Route path={'/users'} component={Users}/>
+                    <Route path={'/posts'} component={Posts}/>
+                    <Route path={'/comments'} component={Comments}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 
